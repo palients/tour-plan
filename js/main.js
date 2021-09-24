72b8356b4210098ff80fcb107a3c0b04bcb7e394
palients <tacegml@gmail.com>
2021-09-24 16:58:13 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+  speed: 500,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
+  },
+});
