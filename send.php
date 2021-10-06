a59698f1dc6fc24882dc34be923832e75949a32b
palients <tacegml@gmail.com>
2021-10-06 20:31:01 +0300

    fix: adaptive footer block

--- send.php
+++ send.php
@@ -1,23 +1,23 @@
 <?php
 // Файлы phpmailer
-require 'phpmailer/PHPMailer.php';
-require 'phpmailer/SMTP.php';
-require 'phpmailer/Exception.php';
+require "phpmailer/PHPMailer.php";
+require "phpmailer/SMTP.php";
+require "phpmailer/Exception.php";
 
 // Переменные, которые отправляет пользователь
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-$email = $_POST['email'];
+$name = $_POST["name"];
+$phone = $_POST["phone"];
+$message = $_POST["message"];
+$email = $_POST["email"];
 
 // Формирование самого письма
-$title = "Новое обращение Best Tour Plan";
+$title = "New appeal Best Tour Plan";
 $body = "
-<h2>Новое обращение</h2>
-<b>Имя:</b> $name<br>
-<b>Электронная почта:</b><br>$email<br>
-<b>Телефон:</b> $phone<br><br>
-<b>Сообщение:</b><br>$message
+<h2>New appeal</h2>
+<b>Name:</b> $name<br>
+<b>email:</b><br>$email<br>
+<b>Phone:</b> $phone<br><br>
+<b>Message:</b><br>$message
 
 ";
 
@@ -27,19 +27,19 @@ try {
     $mail->isSMTP();   
     $mail->CharSet = "UTF-8";
     $mail->SMTPAuth   = true;
-    $mail->SMTPDebug = 2;
-    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+    // $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS["status"][] = $str;};
 
     // Настройки вашей почты
-    $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
-    $mail->Username   = '###'; // Логин на почте
-    $mail->Password   = '###'; // Пароль на почте
-    $mail->SMTPSecure = 'ssl';
+    $mail->Host       = "smtp.yandex.ru"; // SMTP сервера вашей почты
+    $mail->Username   = "c.morozov@coffeestudio.ru"; // Логин на почте
+    $mail->Password   = "2021Constantin2021"; // Пароль на почте
+    $mail->SMTPSecure = "ssl";
     $mail->Port       = 465;
-    $mail->setFrom('###', 'Constantin Morozov'); // Адрес самой почты и имя отправителя
+    $mail->setFrom("c.morozov@coffeestudio.ru", "Constantin Morozov"); // Адрес самой почты и имя отправителя
 
     // Получатель письма
-    $mail->addAddress('palients@yandex.ru');
+    $mail->addAddress("palients@yandex.ru");
 
 // Отправка сообщения
 $mail->isHTML(true);
@@ -56,4 +56,4 @@ else {$result = "error";}
 }
 
 // Отображение результата
-header('Location: thankyou.html');
+header("Location: thankyou.html");
